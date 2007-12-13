65156790ae5dc5a8c5b728755f41bf47bda3d3ab
Bob Aman <bob@sporkmonger.com>
2007-12-13 17:33:58 -0500

    Updated copyright notice.

--- lib/gentlerest/version.rb
+++ lib/gentlerest/version.rb
@@ -1,5 +1,5 @@
 #--
-# GentleREST, Copyright (c) 2007 Bob Aman
+# GentleREST, Copyright (c) 2007-2008 Bob Aman
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
