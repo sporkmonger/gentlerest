dfd14fff5ebb05c093f940e8beb4a6036f52d208
Bob Aman <bob@sporkmonger.com>
2007-12-13 17:34:47 -0500

    Removed unused vendor directory from .

--- lib/gentlerest.rb
+++ lib/gentlerest.rb
@@ -22,8 +22,6 @@
 #++
 
 $:.unshift(File.expand_path(File.dirname(__FILE__)))
-$:.unshift(File.expand_path(
-  File.join(File.dirname(__FILE__), "../vendor/haml/lib")))
 $:.uniq!
 
 require 'rubygems'
