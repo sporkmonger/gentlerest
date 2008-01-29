a93540a66fd5f3144d77ef0ce7da0d53bf4bc69d
Bob Aman <bob@sporkmonger.com>
2008-01-29 16:59:27 -0500

    The test server now defaults to port 3500 instead of 3000.

--- spec/gentlerest/init_server.rb
+++ spec/gentlerest/init_server.rb
@@ -2,7 +2,7 @@ require 'gentlerest'
 
 if GentleREST.server(:default) == nil
   Thread.new do
-    GentleREST.start(:name => :default)
+    GentleREST.start(:name => :default, :port => 3500)
   end
 end
 loop do
